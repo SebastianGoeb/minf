7877bfe307ab49cc9ee0c133ace47cd2c9b7426f
Sebastian Goeb <sebastiangoeb@hotmail.com>
2016-10-29 16:04:14 +0100

    Switch disconnect and binary tree updates

---
+++ src/test/java/net/floodlightcontroller/serverloadbalancer/SwitchDisconnectTest.java
@@ -0,0 +1,13 @@
+package net.floodlightcontroller.serverloadbalancer;
+
+import net.floodlightcontroller.core.OFSwitch;
+import net.floodlightcontroller.core.internal.MockOFSwitchImpl;
+import org.junit.Test;
+
+public class SwitchDisconnectTest {
+
+    @Test
+    public void testGenerateIPv4AssignmentTree() throws Exception {
+        OFSwitch sw = new MockOFSwitchImpl();
+    }
+}
