54382ab5e501530176e2405ec0afc1994c26cb12
Sebastian Goeb <sebastiancgoeb@gmail.com>
2017-01-30 19:25:17 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -2,20 +2,37 @@
 
 dir=$(cd "$(dirname "$0")"; pwd)
 
-# Build controller
 build_controller()
 {
-    echo "**** BUILDING CONTROLLER ****"
+    echo -e "\033[1m**** Building Controller ****\033[0m"
     cd "$dir/floodlight"
     ant
+    echo
 }
 
-# Build server
+build_driver()
+{
+    echo -e "\033[1m**** Building Driver ****\033[0m"
+    cd "$dir/driver"
+    mvn clean install
+    echo
+}
+
 build_server()
 {
-    echo "**** BUILDING SERVER ****"
+    echo -e "\033[1m**** Building Server ****\033[0m"
     cd "$dir/server"
     mvn clean install
+    echo
 }
 
-echo "unfinished script!"
+while getopts "cds" opt; do
+     case "$opt" in
+         c)  build_controller
+             ;;
+         d)  build_driver
+             ;;
+         s)  build_server
+             ;;
+     esac
+ done
