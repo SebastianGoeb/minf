0dd6704d2c73ab5e5915c8cda8243ce1f2668fe1
Sebastian Goeb <sebastiancgoeb@gmail.com>
2017-02-25 12:36:45 +0000

    Improved logging of setup.sh

--- setup.sh
+++ setup.sh
@@ -1,44 +1,54 @@
 #!/bin/bash
 
+# Utils
+function echobold()
+{
+	echo -e "\033[1m$1\033[0m"
+}
+
+# Find base dir
 dir=$(cd "$(dirname "$0")"; pwd)
 
-echo "**** SETTING UP FLOODLIGHT CONTROLLER ****"
-cd $dir
-
-# Clone
+# Clone or clean floodlight
 if [[ ! -d floodlight ]]; then
+	echobold "Cloning fresh floodlight repo"
+	cd $dir
     rm -rf floodlight
     git clone git@github.com:floodlight/floodlight.git floodlight
 else
-    cd "$dir/floodlight"
-    git reset --hard
-    git clean -fd
+	echobold "Cleaning floodlight directory"
+	cd "$dir/floodlight"
+	git reset --hard
+	git clean -fd
 fi
 
 # Checkout consistent version
 # TODO tested to work with this version of Floodlight. Use release version of Floodlight when available
+echobold "\nChecking out a commit that is known to work"
 cd "$dir/floodlight"
 git checkout bdddcb4810c04a3eef77c54567ee6728d1674162
 
 # Init submodules
+echobold "\n Initializing git submodules"
 git submodule update --init
 
 # Create symlinks to custom controller code
+echobold "\nCreating symlinks to custom controller code"
 paths=(
 	"src/main/resources/floodlightloadbalancer.properties"
 	"src/main/resources/META-INF/services/net.floodlightcontroller.core.module.IFloodlightModule"
 	"src/main/java/net/floodlightcontroller/proactiveloadbalancer"
 )
 for path in ${paths[@]}; do
+	echo "./$path -> $dir/controller/$path"
 	[ -f $path ] && rm $path
 	[ -d $path ] && rm -rf $path
 	ln -s "$dir/controller/$path" $path
 done
 
 # Setup eclipse project
+echobold "\nCreating eclipse project"
 ant eclipse
-
-# Replace .project file (include linked resources)
 cat >.project <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <projectDescription>
